0ae618ae7d82f8b536ac9ed3ecfeabcb67fee989
matt765 <63603595+matt765@users.noreply.github.com>
2021-05-03 19:11:05 +0200

    Create tile_teamwork_tactics.js

---
+++ tile_teamwork_tactics.js
@@ -0,0 +1,21 @@
+// Tile Teamwork Tactics
+// Source: edabit.com
+
+// In a board game, a piece may advance 1-6 tiles forward depending on the number rolled on a six-sided dice. 
+// If you advance your piece onto the same tile as another player's piece, both of you earn a bonus.
+// Given you and your friend's tile number, create a function that returns if it's possible to earn a bonus when you roll the dice.
+// You cannot move backward. If you are already on the same tile, return false, as you would be advancing away. Expect only positive integer inputs.
+
+function possibleBonus(first, second) {
+  let dice = [1, 2, 3, 4, 5, 6];
+  let newDice = dice.filter((number) => {
+    return first + number == second;
+  })
+   
+  if (newDice.length !== 0) {
+    return true
+  }
+  else {
+    return false
+  }
+}
