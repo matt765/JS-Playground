fff9ad85c527b869eb6e79e361d1ea0323958c1b
matt765 <63603595+matt765@users.noreply.github.com>
2021-05-15 21:33:27 +0200

    Update tile-teamwork-tactics.js

--- tile-teamwork-tactics.js
+++ tile-teamwork-tactics.js
@@ -1,5 +1,5 @@
 // Tile Teamwork Tactics
-// Source: edabit.com
+// Source of instructions: https://edabit.com/challenge/NHfYRHg2tDtcZyykB
 
 // In a board game, a piece may advance 1-6 tiles forward depending on the number rolled on a six-sided dice. 
 // If you advance your piece onto the same tile as another player's piece, both of you earn a bonus.
