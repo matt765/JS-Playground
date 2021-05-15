ed3777de5be73978f6745b6129db6f92e40ff747
matt765 <63603595+matt765@users.noreply.github.com>
2021-05-15 21:28:32 +0200

    Update nested-array-length.js

--- nested-array-length.js
+++ nested-array-length.js
@@ -1,5 +1,5 @@
 // Length of a Nested Array
-// Source: Edabit.com
+// Source of instructions: https://edabit.com/challenge/yXSTvCNen2DQHyrh6
 
 // The .length property on an array will return the number of elements in the array.
 // Suppose we instead wanted to know the total number of non-nested items in the nested array. In the above case, [1, [2, 3]] contains 3 non-nested items, 1, 2 and 3.
